97d783b294cbd6437c6eec9f1549480eaa0a4082
madison <longmadison@gmail.com>
2025-11-17 21:18:11 -0800

    trying new solution with deploy.yml and viteconfig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  // Use the repo name as the base path when building for GitHub Pages.
+  base: command === 'build' ? '/project2_madisonlong1/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+}));
